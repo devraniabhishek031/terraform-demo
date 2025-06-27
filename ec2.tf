26e604597fbab0d1522076b2d1906faff503bffa
devraniabhishek031 <156890415+devraniabhishek031@users.noreply.github.com>
2025-06-27 10:27:35 +0530

    Create ec2.tf

---
+++ ec2.tf
@@ -0,0 +1,27 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"] # Canonical
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+resource "aws_instance" "ubuntu" {
+  ami                         = data.aws_ami.ubuntu.id
+  instance_type               = var.instance_type
+  subnet_id                   = aws_subnet.public.id
+  vpc_security_group_ids      = [aws_security_group.instance_sg.id]
+  associate_public_ip_address = true
+  key_name                    = "testing" # Replace with your SSH key
+  tags = {
+    Name = "UbuntuInstance"
+  }
+}
+
